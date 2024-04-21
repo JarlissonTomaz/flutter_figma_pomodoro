8216ae4e3223191b34566f7a2c9bad8fa4e5fedc
JarlissonTomaz <tomazj995@gmail.com>
2024-04-21 17:10:48 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/br/tom/flutter_figma_pomodoro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.br.tom.flutter_figma_pomodoro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home App'),
+      ),
+      body: const Text('HomeApp'),
+    );
+  }
+}
